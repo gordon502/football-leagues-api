9b976bee751dbc54b619a1a2f0200401af9adc99
gordon502 <gordonarek@gmail.com>
2024-04-03 20:46:01 +0200

    handle JWT Not Found exception

--- src/Modules/Auth/EventListener/JwtAuthenticationFailureListener.php
+++ src/Modules/Auth/EventListener/JwtAuthenticationFailureListener.php
@@ -5,6 +5,7 @@ namespace App\Modules\Auth\EventListener;
 use App\Common\Response\ErrorResponse;
 use App\Common\Response\HttpCode;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTNotFoundEvent;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class JwtAuthenticationFailureListener
@@ -15,6 +16,19 @@ class JwtAuthenticationFailureListener
             HttpCode::UNAUTHORIZED,
             'INVALID_CREDENTIALS',
             'Invalid credentials, could not log you in.'
+        );
+
+        $response = new JsonResponse($error, $error->code);
+
+        $event->setResponse($response);
+    }
+
+    public function onJwtNotFound(JWTNotFoundEvent $event): void
+    {
+        $error = new ErrorResponse(
+            HttpCode::UNAUTHORIZED,
+            'JWT_NOT_FOUND',
+            'JWT token not found.'
         );
 
         $response = new JsonResponse($error, $error->code);
