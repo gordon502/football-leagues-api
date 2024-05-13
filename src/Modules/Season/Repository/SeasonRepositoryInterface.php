13d6bb9c6b6e198ae49d80dc5fce954a6f433455
gordon502 <gordonarek@gmail.com>
2024-05-13 22:01:10 +0200

    SeasonRepositoryInterface.php typing

--- src/Modules/Season/Repository/SeasonRepositoryInterface.php
+++ src/Modules/Season/Repository/SeasonRepositoryInterface.php
@@ -4,7 +4,9 @@ namespace App\Modules\Season\Repository;
 
 use App\Common\Repository\DeletableByIdInterface;
 use App\Common\Repository\FindableByIdInterface;
+use App\Modules\Season\Model\SeasonInterface;
 
 interface SeasonRepositoryInterface extends FindableByIdInterface, DeletableByIdInterface
 {
+    public function findById(string $id): ?SeasonInterface;
 }
