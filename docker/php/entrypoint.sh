49538c57a53936ee671078ac3140bae261f7099d
gordon502 <gordonarek@gmail.com>
2024-09-21 13:03:19 +0200

    again increase php-fpm settings

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,8 +1,8 @@
 # shellcheck disable=SC2129
 echo 'pm.max_children = 151' >> /usr/local/etc/php-fpm.d/www.conf
 echo 'pm.max_requests = 1000' >> /usr/local/etc/php-fpm.d/www.conf
-echo 'pm.start_servers = 50' >> /usr/local/etc/php-fpm.d/www.conf
-echo 'pm.min_spare_servers = 50' >> /usr/local/etc/php-fpm.d/www.conf
-echo 'pm.max_spare_servers = 50' >> /usr/local/etc/php-fpm.d/www.conf
+echo 'pm.start_servers = 151' >> /usr/local/etc/php-fpm.d/www.conf
+echo 'pm.min_spare_servers = 151' >> /usr/local/etc/php-fpm.d/www.conf
+echo 'pm.max_spare_servers = 151' >> /usr/local/etc/php-fpm.d/www.conf
 
 docker-php-entrypoint php-fpm
